13ea35c5114433a7a3f5c91ab55fe62fc2cb5abc
Kumiko Kashii <kkashii@uyahoo.com>
2017-12-17 22:22:02 -0800

    Add random input modify

--- kaggle_speech_recog/graphs/__init__.py
+++ kaggle_speech_recog/graphs/__init__.py
@@ -3,6 +3,7 @@ from .train_log import *
 
 from .useful_tf_graph import *
 
+from .spectrogram_multi_lstm_random_input_modify import *
 from .spectrogram_concat_conv_lstm import *
 from .spectrogram_bn_multi_lstm import *
 from .spectrogram_multi_lstm import *
