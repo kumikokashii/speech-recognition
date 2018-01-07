a2ca95755be5320fd7e1164bfd911be71c741a0b
Kumiko Kashii <kkashii@uyahoo.com>
2018-01-07 12:37:54 -0800

    Add CLSTM and DS Conv

--- kaggle_speech_recog/graphs/__init__.py
+++ kaggle_speech_recog/graphs/__init__.py
@@ -5,6 +5,11 @@ from .useful_tf_graph import *
 
 from .novelty_detection_spectrogram_multi_lstm_random_input_modify import *
 
+from .spectrogram_ds_conv_drop import *
+from .spectrogram_ds_conv import *
+from .spectrogram_clstm_bidirectional import *
+from .spectrogram_clstm import *
+from .spectrogram_maxpool_lstm import *
 from .spectrogram_multi_lstm_random_input_modify import *
 from .spectrogram_concat_conv_lstm import *
 from .spectrogram_bn_multi_lstm import *
