59b433eb496074da59c8fed3097d98ae296f7c96
kumikokashii <kkashii@uyahoo.com>
2018-01-10 18:11:42 -0800

    Add to graphs init

--- kaggle_speech_recog/graphs/__init__.py
+++ kaggle_speech_recog/graphs/__init__.py
@@ -5,6 +5,7 @@ from .useful_tf_graph import *
 
 from .novelty_detection_spectrogram_multi_lstm_random_input_modify import *
 
+from .spectrogram_concat_clstm_v2 import *
 from .spectrogram_concat_clstm import *
 from .spectrogram_ds_conv_drop import *
 from .spectrogram_ds_conv import *
